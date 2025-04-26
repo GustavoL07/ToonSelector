20962dec1c53284c48e09da2ce6549b0f08d4a84
Gustavo Lepinsk Carvalho <gustavolepinskcarvalho07@gmail.com>
2025-04-26 14:29:42 -0300

    #

--- src/toons.js
+++ src/toons.js
@@ -7,41 +7,41 @@ class Toon {
 
 
 const toonsList = [
-  new Toon("Boxten", "/assets/toon-images/Boxten_Render.webp"),
-  new Toon("Brightney", "/assets/toon-images/Brightney_Render.webp"),
-  new Toon("Connie", "/assets/toon-images/Connie_Render.webp"),
-  new Toon("Cosmo", "/assets/toon-images/Cosmo_Render.webp"),
-  new Toon("Finn", "/assets/toon-images/Finn_Render.webp"),
-  new Toon("Flutter", "/assets/toon-images/Flutter_Render.webp"),
-  new Toon("Gigi", "/assets/toon-images/Gigi_Render.webp"),
-  new Toon("Glisten", "/assets/toon-images/Glisten_Render.webp"),
-  new Toon("Goob", "/assets/toon-images/Goob_Render.webp"),
-  new Toon("Looey", "/assets/toon-images/Looey_Render.webp"),
-  new Toon("Poppy", "/assets/toon-images/Poppy_Render.webp"),
-  new Toon("Razzel & Dazzel", "/assets/toon-images/RazzleDazzle_Render.webp"),
-  new Toon("Rodger", "/assets/toon-images/Rodger_Render.webp"),
-  new Toon("Scraps", "/assets/toon-images/Scraps_Render.webp"),
-  new Toon("Shrimpo", "/assets/toon-images/Shrimpo_Render.webp"),
-  new Toon("Teagan", "/assets/toon-images/Teagan_Render.webp"),
-  new Toon("Tisha", "/assets/toon-images/Tisha_Render.webp"),
-  new Toon("Toodles", "/assets/toon-images/Toodles_Render.webp"),
-  new Toon("Yatta", "/assets/toon-images/Yatta_Render.webp"),
+  new Toon("Boxten", "/ToonSelector/assets/toon-images/Boxten_Render.webp"),
+  new Toon("Brightney", "/ToonSelector/assets/toon-images/Brightney_Render.webp"),
+  new Toon("Connie", "/ToonSelector/assets/toon-images/Connie_Render.webp"),
+  new Toon("Cosmo", "/ToonSelector/assets/toon-images/Cosmo_Render.webp"),
+  new Toon("Finn", "/ToonSelector/assets/toon-images/Finn_Render.webp"),
+  new Toon("Flutter", "/ToonSelector/assets/toon-images/Flutter_Render.webp"),
+  new Toon("Gigi", "/ToonSelector/assets/toon-images/Gigi_Render.webp"),
+  new Toon("Glisten", "/ToonSelector/assets/toon-images/Glisten_Render.webp"),
+  new Toon("Goob", "/ToonSelector/assets/toon-images/Goob_Render.webp"),
+  new Toon("Looey", "/ToonSelector/assets/toon-images/Looey_Render.webp"),
+  new Toon("Poppy", "/ToonSelector/assets/toon-images/Poppy_Render.webp"),
+  new Toon("Razzel & Dazzel", "/ToonSelector/assets/toon-images/RazzleDazzle_Render.webp"),
+  new Toon("Rodger", "/ToonSelector/assets/toon-images/Rodger_Render.webp"),
+  new Toon("Scraps", "/ToonSelector/assets/toon-images/Scraps_Render.webp"),
+  new Toon("Shrimpo", "/ToonSelector/assets/toon-images/Shrimpo_Render.webp"),
+  new Toon("Teagan", "/ToonSelector/assets/toon-images/Teagan_Render.webp"),
+  new Toon("Tisha", "/ToonSelector/assets/toon-images/Tisha_Render.webp"),
+  new Toon("Toodles", "/ToonSelector/assets/toon-images/Toodles_Render.webp"),
+  new Toon("Yatta", "/ToonSelector/assets/toon-images/Yatta_Render.webp"),
 
-  new Toon("Astro", "/assets/toon-images/Astro_Render.webp"),
-  new Toon("Bassie", "/assets/toon-images/Bassie_Render.webp"),
-  new Toon("Bobette", "/assets/toon-images/Bobette_Render.webp"),
-  new Toon("Pebble", "/assets/toon-images/Pebble_Render.webp"),
-  new Toon("Shelly", "/assets/toon-images/Shelly_Render.webp"),
-  new Toon("Sprout", "/assets/toon-images/Sprout_Render.webp"),
-  new Toon("Vee", "/assets/toon-images/Vee_Render.webp"),
+  new Toon("Astro", "/ToonSelector/assets/toon-images/Astro_Render.webp"),
+  new Toon("Bassie", "/ToonSelector/assets/toon-images/Bassie_Render.webp"),
+  new Toon("Bobette", "/ToonSelector/assets/toon-images/Bobette_Render.webp"),
+  new Toon("Pebble", "/ToonSelector/assets/toon-images/Pebble_Render.webp"),
+  new Toon("Shelly", "/ToonSelector/assets/toon-images/Shelly_Render.webp"),
+  new Toon("Sprout", "/ToonSelector/assets/toon-images/Sprout_Render.webp"),
+  new Toon("Vee", "/ToonSelector/assets/toon-images/Vee_Render.webp"),
 
-  new Toon("Cocoa", "/assets/toon-images/Cocoa_Render.webp"),
-  new Toon("Eggson", "/assets/toon-images/Eggson_Render.webp"),
-  new Toon("Flyte", "/assets/toon-images/Flyte_Render.webp"),
+  new Toon("Cocoa", "/ToonSelector/assets/toon-images/Cocoa_Render.webp"),
+  new Toon("Eggson", "/ToonSelector/assets/toon-images/Eggson_Render.webp"),
+  new Toon("Flyte", "/ToonSelector/assets/toon-images/Flyte_Render.webp"),
 
-  new Toon("Coal", "/assets/toon-images/Coal_Render.webp"),
-  new Toon("Ginger", "/assets/toon-images/Ginger_Render.webp"),
-  new Toon("Rudie", "/assets/toon-images/Rudie_Render.webp"),
+  new Toon("Coal", "/ToonSelector/assets/toon-images/Coal_Render.webp"),
+  new Toon("Ginger", "/ToonSelector/assets/toon-images/Ginger_Render.webp"),
+  new Toon("Rudie", "/ToonSelector/assets/toon-images/Rudie_Render.webp"),
 ]
 
 export function getRandomToon() {
@@ -49,4 +49,4 @@ export function getRandomToon() {
   return toonsList[randomIndex];
 }
 
-export const DANDY = new Toon("Dandy", "/assets/toon-images/Dandy_Render.webp");
+export const DANDY = new Toon("Dandy", "/ToonSelector/assets/toon-images/Dandy_Render.webp");
